2f83c946851cdb9ed6ef6c4fa81c28f85d238e01
Yevgeniy Levchuk <elevchuk@gmail.com>
2026-03-05 16:32:57 +0500

    feat: select warm stone as default dark theme

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -7,5 +7,11 @@ export function ThemeProvider({
     children,
     ...props
 }: ThemeProviderProps) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    return (
+        <NextThemesProvider
+            {...props}
+        >
+            {children}
+        </NextThemesProvider>
+    )
 }
